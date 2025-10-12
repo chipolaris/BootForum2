615b4f90fd0d7585c85b657fbe215394901147e0
chipolaris <ch3.nguyen@gmail.com>
2025-10-12 12:50:13 -0400

    Dark mode enhancement for login, registration, resource-not-found, file-list, and discussion components

--- src/main/ngapp/src/app/app.config.ts
+++ src/main/ngapp/src/app/app.config.ts
@@ -31,7 +31,10 @@ export const appConfig: ApplicationConfig = {
     provideAnimationsAsync(),
     providePrimeNG({
       theme: {
-        preset: Aura
+        preset: Aura,
+        options: {
+          darkModeSelector: '.dark',
+        },
       }
     }),
     // APP_INITIALIZER provider
